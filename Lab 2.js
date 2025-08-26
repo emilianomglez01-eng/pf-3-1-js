09ebd6d913a0ad243bb5d866fdb86bea34a2bfa3
Emiliano Muñoz <emilianom01@gmail.com>
2025-08-26 10:53:23 -0600

    Ejercisio 2 inconcluso

---
+++ Lab 2.js
@@ -0,0 +1,9 @@
+export function addTwoNumbers(a=5, b=7){
+  //aqui va tu codigo 
+  console.log(a+b)
+  console.log(-10 + a)
+  console.log(34 + a)
+  console.log(15 + a)
+  return a + b
+}
+console.log(addTwoNumbers() + " es la suma de 5 + 7")
